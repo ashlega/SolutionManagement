504adb90052c0a873cbe68cca0b65e9517ee87c0
ashlega@yahoo.com <ashlega@yahoo.com>
2018-08-09 20:33:34 -0400

    Replacing workflow activity with a plugin to avoid Generic Sql Error

--- ItAintBoring.SolutionManagement/DeleteAttributeActivity.cs
+++ ItAintBoring.SolutionManagement/DeleteAttributeActivity.cs
@@ -10,48 +10,68 @@ using Microsoft.Xrm.Sdk.Messages;
 
 namespace ItAintBoring.SolutionManagement.Workflow.Activities
 {
-    public sealed class DeleteAttributeActivity : CodeActivity
-    {
+    /*
+   public sealed class DeleteAttributeActivity : CodeActivity
+   {
 
-        /// <summary>
-        /// Executes the workflow activity.
-        /// </summary>
-        /// <param name="executionContext">The execution context.</param>
-        protected override void Execute(CodeActivityContext executionContext)
-        {
-            ITracingService tracingService = executionContext.GetExtension<ITracingService>();
-            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
-            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
-            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+       /// <summary>
+       /// Executes the workflow activity.
+       /// </summary>
+       /// <param name="executionContext">The execution context.</param>
+       protected override void Execute(CodeActivityContext executionContext)
+       {
+           ITracingService tracingService = executionContext.GetExtension<ITracingService>();
+           IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+           IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+           IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            var entity = (string)executionContext.GetValue(Entity);
-            var attribute = (string)executionContext.GetValue(Attribute);
+           var entity = (string)executionContext.GetValue(Entity);
+           var attribute = (string)executionContext.GetValue(Attribute);
 
-            Entity ent = new Entity(context.PrimaryEntityName);
-            ent.Id = context.PrimaryEntityId;
-            try
-            {
-                DeleteAttributeRequest dar = new DeleteAttributeRequest();
-                dar.EntityLogicalName = entity;
-                dar.LogicalName = attribute;
-                service.Execute(dar);
-                ent["statuscode"] = new OptionSetValue(455000000);
-            }
-            catch(Exception ex)
-            {
-                ent["ita_details"] = ex.Message;
-                ent["statuscode"] = new OptionSetValue(455000001);
-            }
-            if (context.PrimaryEntityName.ToUpper() == "ITA_SOLUTIONACTION")//Can be used on a different entity
-            {
-                service.Update(ent);
-            }
-        }
+           Entity ent = new Entity("ita_solutionaction");
+           ent.Id = new Guid("d7eea101-b796-457a-8c2a-4ad8fca48cc3");
 
-        [Input("Entity")]
-        public InArgument<string> Entity { get; set; }
+           bool updated = false;
+           while (!updated)
+           {
+               try
+               {
+                   service.Update(ent);
+                   updated = true;
+               }
+               catch (Exception ex)
+               {
+                   service.Create(ent);
+               }
+           }
 
-        [Input("Attribute")]
-        public InArgument<string> Attribute { get; set; }
-    }
+           ent = new Entity(context.PrimaryEntityName);
+           ent.Id = context.PrimaryEntityId;
+           try
+           {
+
+               DeleteAttributeRequest dar = new DeleteAttributeRequest();
+               dar.EntityLogicalName = entity;
+               dar.LogicalName = attribute;
+               service.Execute(dar);
+               ent["statuscode"] = new OptionSetValue(455000000);
+           }
+           catch(Exception ex)
+           {
+               ent["ita_details"] = ex.Message;
+               ent["statuscode"] = new OptionSetValue(455000001);
+           }
+           if (context.PrimaryEntityName.ToUpper() == "ITA_SOLUTIONACTION")//Can be used on a different entity
+           {
+               service.Update(ent);
+           }
+       }
+
+       [Input("Entity")]
+       public InArgument<string> Entity { get; set; }
+
+       [Input("Attribute")]
+       public InArgument<string> Attribute { get; set; }
+   }
+   */
 }
---
+++ ItAintBoring.SolutionManagement/DeleteAttributePlugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Messages;
+
+namespace ItAintBoring.SolutionManagement
+{
+    public class DeleteAttributePlugin : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            var ent =(Entity)context.InputParameters["Target"];
+            string entity = ent.Contains("ita_entity") ? (string)ent["ita_entity"] : null;
+            string attribute = ent.Contains("ita_attribute") ? (string)ent["ita_attribute"] : null;
+            if (entity != null && attribute != null)
+            {
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+
+                    DeleteAttributeRequest dar = new DeleteAttributeRequest();
+                    dar.EntityLogicalName = entity;
+                    dar.LogicalName = attribute;
+                    service.Execute(dar);
+                    ent["statuscode"] = new OptionSetValue(455000000);
+                }
+                catch (Exception ex)
+                {
+                    ent["ita_details"] = ex.Message;
+                    ent["statuscode"] = new OptionSetValue(455000001);
+                }
+
+            }
+
+        }
+    }
+}
